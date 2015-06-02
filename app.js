17ef0e81f34672d646e2f443f8690cf5d39045ce
Lucia Santos <analucia0189@gmail.com>
2015-06-02 02:04:25 -0600

    routes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var noticias = require('./routes/noticias');
+var bars = require('./routes/bars');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -24,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/noticias', noticias);
+app.use('/bars', bars);
+app.user('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Janover' });
+  res.render('about', { title: 'About - Janover' });
 });
 
 module.exports = router;
