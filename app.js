f10a404e567ead7417cc25a99f3b973f8ac3fc2b
Lucia Santos <analucia0189@gmail.com>
2015-06-02 02:09:33 -0600

    fix nothing

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/noticias', noticias);
 app.use('/bars', bars);
-app.user('/about', about);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
